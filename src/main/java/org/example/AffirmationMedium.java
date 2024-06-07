29a88b17e64ef478067a12171a5984736045cd6c
Voahary Mampionona <hei.zoarisoa@gmail.com>
2024-06-07 16:31:51 +0300

    feat: add new function in MediumAffirmation

--- src/main/java/org/example/AffirmationMedium.java
+++ src/main/java/org/example/AffirmationMedium.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 import org.example.enums.VerityValeur;
 
+@Getter
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
 public class AffirmationMedium extends Affirmation{
     private final Affirmation rightAffirmation;
     private final Affirmation leftAffirmation;
