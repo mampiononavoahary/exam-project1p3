b168f05545cf7e16a4272988fd8f5cccfa46906f
Voahary Mampionona <hei.zoarisoa@gmail.com>
2024-06-07 15:16:28 +0300

    feat: add the body affirmation to implements the different affirmation

--- src/main/java/org/example/Affirmation.java
+++ src/main/java/org/example/Affirmation.java
@@ -1,4 +1,4 @@
-package org.example.ConnecteurLogiqueEtNegation;
+package org.example;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -6,12 +6,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.example.enums.VerityValeur;
 
-@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @EqualsAndHashCode
 public abstract class Affirmation {
     private final String affirmatiobBody;
-
     public abstract VerityValeur valeur();
 }
