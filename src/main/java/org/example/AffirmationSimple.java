90a2ca14d94ffc1a65deec15e8bbbe759ac8f23e
Voahary Mampionona <hei.zoarisoa@gmail.com>
2024-06-07 15:17:12 +0300

    feat: add the simple affirmation

--- src/main/java/org/example/AffirmationSimple.java
+++ src/main/java/org/example/AffirmationSimple.java
@@ -1,2 +1,19 @@
-package org.example;public class AffirmationSimple {
+package org.example;
+
+import lombok.Getter;
+import org.example.enums.VerityValeur;
+
+@Getter
+public class AffirmationSimple extends Affirmation{
+    private final VerityValeur verityValeur;
+
+    public AffirmationSimple(String affiramtionBody, VerityValeur verityValeur) {
+        super(affiramtionBody);
+        this.verityValeur = verityValeur;
+    }
+
+    @Override
+    public VerityValeur valeur() {
+        return verityValeur;
+    }
 }
